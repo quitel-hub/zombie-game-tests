2d3f61e360f81770b22c30de0bc1a9c13e4902f8
quitel-hub <karpenko.k.a.07@gmail.com>
2025-10-09 18:18:04 +0200

    Add unit tests and refactor Game logic

---
+++ Command.h
@@ -0,0 +1,8 @@
+//
+// Created by quitel on 09.10.2025.
+//
+
+#ifndef UNTITLED23_COMMAND_H
+#define UNTITLED23_COMMAND_H
+
+#endif //UNTITLED23_COMMAND_H
---
+++ Game.cpp
@@ -0,0 +1,3 @@
+//
+// Created by quitel on 09.10.2025.
+//
