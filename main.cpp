f999dc37c7b28291fd740125b1f83478a5af4842
quitel-hub <karpenko.k.a.07@gmail.com>
2025-10-09 16:48:43 +0200

    Setup .gitignore and untrack build/IDE files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #define _WIN32_WINNT 0x0A00
 #define WIN32_LEAN_AND_MEAN
 
+#include <SFML/Graphics.hpp>
 #include <windows.h>
 #include<cstdlib>
 #include<iostream>
@@ -14,6 +15,30 @@ using namespace std;
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+//    sf::RenderWindow window(sf::VideoMode(800, 600), "Zombie Survival");
+//    Game game;
+//    while (window.isOpen()) {
+//        // Обробка подій (натискання клавіш, закриття вікна)
+//        sf::Event event;
+//        while (window.pollEvent(event)) {
+//            if (event.type == sf::Event::Closed) {
+//                window.close();
+//            }
+//            // Тут буде обробка натискань клавіш
+//        }
+//        // Оновлення логіки гри
+//        game.update();
+//
+//        // Очищення вікна
+//        window.clear();
+//
+//        // Малювання ігрових об'єктів
+//        game.render(window);
+//
+//        // Відображення намальованого на екрані
+//        window.display();
+//    }
+
     string lang;
     cout << "Choose language (en/ukr):";
     cin >> lang;
